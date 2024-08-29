16854afc08b83931edbcb45dbb039e78efd76306
Prasanth-S7 <prasanthsampath2005@gmail.com>
2024-08-29 09:37:55 +0530

    added popover

--- client/src/components/mine/LBar/Lbar.tsx
+++ client/src/components/mine/LBar/Lbar.tsx
@@ -1,23 +1,43 @@
 import './LBar.css'
 import { BackpackIcon, ExclamationTriangleIcon, FileTextIcon, HomeIcon, RocketIcon } from '@radix-ui/react-icons';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import NavMenu from '../NavMenu/NavMenu';
-import { Card, CardContent } from '@/components/ui/card';
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+
 const LBar = () => {
-  return(
+  return (
     <>
-      <div className="bg-white horizontal-bar flex flex-row justify-between items-center p-1">
-        <div className="left-content">
+      <div className="bg-white horizontal-bar flex flex-row justify-between items-center p-1 border-b">
+        <div className="">
           App Name
         </div>
         <div className="right-content">
-              <Avatar>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Avatar className='hover:cursor-pointer'>
                 <AvatarImage src="https://github.com/shadcn.png" />
                 <AvatarFallback>CN</AvatarFallback>
               </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem>Support</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
-      <div className="vertical-bar bg-white">
+      <div className="vertical-bar border-r bg-white">
         <div className="logo-container h-[30px] w-[30px] bg-slate-700 rounded-full mx-auto my-3">
           {/* Place moss logo here */}
         </div>

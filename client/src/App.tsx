3b92569a78fb9277ce5850f560a9579ddeb026a7
Prasanth-S7 <prasanthsampath2005@gmail.com>
2024-08-29 08:25:02 +0530

    resolved conflicts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Login from './pages/Login/Login'
@@ -9,68 +8,6 @@ import AssignWorks from './pages/AssignWorks/AssignWorks'
 import MapComponent from './pages/Map/MapComponent'
 import GenerateReport from './pages/GenerateReport/GenerateReport'
 import {ShiftLogs} from "./pages/shiftLogs/shiftLogs"
-import { Alerts } from './pages/alerts/alerts'
-
-function App() {
-  const routes = createBrowserRouter([
-    {
-      path: '/login',
-      index: true,
-      element: <Login />
-    },
-    {
-      path: '/home',
-      element: <Home />
-    },
-    {
-      path: '/',
-      element: <Home />
-    },
-    {
-      path: '/manage-worker',
-      element: <ManageWorker />
-    },
-    {
-      path: '/assign-works',
-      element: <AssignWorks />
-    },
-    {
-      path: '/map',
-      element: <MapComponent />
-    },
-    {
-      path: '/generate-report',
-      element: <GenerateReport />
-    },
-    {
-      path: '/alerts',
-      element: <Alerts />
-    },
-    {
-      path: '/shiftlogs',
-      element: <ShiftLogs/>
-    }
-  ]);
-
-  return (
-    <div className='min-h-screen w-full'>
-      <RouterProvider router={routes} />
-      <Toaster />
-    </div>
-  );
-}
-
-export default App;
-=======
-import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Login from './pages/Login/Login'
-import { Toaster } from './components/ui/sonner'
-import Home from './pages/Home/Home'
-import ManageWorker from './pages/ManageWorkers/ManageWorker'
-import AssignWorks from './pages/AssignWorks/AssignWorks'
-import MapComponent from './pages/Map/MapComponent'
-import GenerateReport from './pages/GenerateReport/GenerateReport'
 import { Alerts } from './pages/alerts/alerts'
 import ShiftPlanning from './pages/ShiftPlanning/ShiftPlanning'
 
@@ -112,6 +49,10 @@ function App() {
     {
       path: '/shift-planning',
       element: <ShiftPlanning />
+    },
+    {
+      path: '/shiftlogs',
+      element: <ShiftLogs/>
     }
   ]);
 
@@ -124,4 +65,3 @@ function App() {
 }
 
 export default App;
->>>>>>> origin/main

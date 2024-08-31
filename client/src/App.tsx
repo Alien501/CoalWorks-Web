60a3daaea4b2b5673d201b0154956ff6ad621e6d
Alien501 <cvignesh404@gmail.com>
2024-08-31 23:21:31 +0530

    checkpoint one

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,6 +12,9 @@ import { Alerts } from './pages/alerts/alerts'
 import ShiftPlanning from './pages/ShiftPlanning/ShiftPlanning'
 import { SensorData } from './pages/sensorsData/sensorData'
 
+import { Webchat, WebchatProvider, Fab, getClient } from "@botpress/webchat";
+import { buildTheme } from "@botpress/webchat-generator";
+import { useState } from 'react'
 function App() {
   const routes = createBrowserRouter([
     {
@@ -60,6 +63,46 @@ function App() {
       element: <SensorData/>
     }
   ]);
+  const { theme, style } = buildTheme({
+    themeName: "eggplant",
+    themeColor: "#634433",
+  });
+
+//Add your Client ID here ⬇️
+const clientId = "75273c02-6b44-4be7-8126-9cee0d1f2cc6";
+const config = {
+    composerPlaceholder: "What would you like to know?",
+    botName: "MineGuard",
+    botAvatar: builderHat,
+    botDescription:
+      "MineGuard is an advanced chatbot tailored for coal mine supervisors to streamline operations, monitor safety, and manage alerts efficiently.",
+    email: {
+      title: "randomEmail@boptress.com",
+      link: "mailto:randomEmail@boptress.com",
+    },
+    phone: {
+      title: "555-555-5555",
+      link: "tel:555-555-5555",
+    },
+    website: {
+      title: "https://botpress.com",
+      link: "https://botpress.com",
+    },
+    termsOfService: {
+      title: "Terms of service",
+      link: "https://botpress.com/terms",
+    },
+    privacyPolicy: {
+      title: "Privacy policy",
+      link: "https://botpress.com/privacy",
+    },
+};
+
+const Bot = () => {
+  return(
+
+  )
+}
 
   return (
     <div className='min-h-screen w-full'>
--- server/app/index.mjs
+++ server/app/index.mjs
@@ -5,19 +5,18 @@ import { configDotenv } from 'dotenv';
 import { handleError } from './utils/handleError.mjs';
 import { logger } from './utils/logger.mjs';
 import { router } from './routes/router.mjs';
-import { initializeWebsocket } from './routes/websocket.mjs';
 
 configDotenv();
 const PORT = process.env.PORT || 3000;
 const app = express();
-const httpServer = createServer(app);
-initializeWebsocket(httpServer)
+// const httpServer = createServer(app);
+// initializeWebsocket(httpServer)
 app.use(express.json());
 app.use(cors());
 app.use(logger);
 app.use('/api/v1', router);
 app.use(handleError);
 
-httpServer.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`HTTP server is running on http://localhost:${PORT}`);
 });

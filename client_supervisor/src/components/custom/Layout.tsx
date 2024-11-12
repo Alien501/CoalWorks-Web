f43591e7ecfa287bc562f52b10715646f4b89794
Alien501 <cvignesh404@gmail.com>
2024-11-12 17:44:03 +0530

    Added page titles

--- client_supervisor/src/components/custom/Layout.tsx
+++ client_supervisor/src/components/custom/Layout.tsx
@@ -1,9 +1,33 @@
-import React from "react"
-import { Outlet } from "react-router-dom"
+import React, { useEffect, useState } from "react"
+import { Outlet, useLocation } from "react-router-dom"
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "../ui/sidebar"
 import { SideNavbar, TopNavbar } from "./Navbars"
 
+interface PageTitle {
+    url: string;
+    title: string;
+}
+
+const pageTitles: PageTitle[] = [
+    { url: '/', title: 'Dashboard' },
+    { url: '/reports', title: 'Reports' },
+    { url: '/rounds', title: 'Round Plans' },
+    { url: '/scheduler', title: 'Scheduler' },
+    { url: '/observations', title: 'Observations' },
+    { url: '/archives', title: 'Archives' },
+    { url: '/templates', title: 'Templates' },
+    { url: '/shift-handover', title: 'Shift Handover' }
+]
+
 const Layout = () => {
+    const [pageTitle, setPageTitle] = useState('');
+    const location = useLocation();
+
+    useEffect(() => {
+        const currentPage = pageTitles.find(page => page.url === location.pathname);
+        setPageTitle(currentPage? currentPage.title: 'Page Not Found')
+    }, [location.pathname])
+    
     return(
         <SidebarProvider>
             <SideNavbar />
@@ -11,7 +35,7 @@ const Layout = () => {
                 <main>
                     <div className="flex items-center p-2 h-16 shadow-lg">
                         <SidebarTrigger />
-                        <TopNavbar pageTitle="Rounds" />
+                        <TopNavbar pageTitle={pageTitle} />
                     </div>
                     <Outlet />
                 </main>

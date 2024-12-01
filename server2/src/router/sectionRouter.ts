82f016cc6afb70c8b28fb0bd97c2a69d6446ec16
Prasanth-S7 <prasanthsampath2005@gmail.com>
2024-12-02 03:01:41 +0530

    server

--- server2/src/router/sectionRouter.ts
+++ server2/src/router/sectionRouter.ts
@@ -1,8 +1,41 @@
 import { Router, Request, Response, NextFunction } from "express";
-import { getSections, newSection } from "../libs/config/section";
+import { getSections, newSection, getAllLargeSections, getAllMediumSections, getAllMicroSections, getAllSectionTypes, getAllSections,getAllSmallSections, getAllUnitSections } from "../libs/config/section";
 import { asyncHandler } from "../utils/asyncHandler";
 
 const sectionRouter = Router();
+
+sectionRouter.post('/create', asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    await newSection(req, res, next);
+}))
+
+sectionRouter.get('/', asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    await getAllSections(req, res, next);
+}))
+
+sectionRouter.get('/items', asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    await getAllSectionTypes(req, res, next);
+}))
+
+sectionRouter.get('/large', asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    await getAllLargeSections(req, res, next);
+}))
+
+sectionRouter.get('/medium', asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    await getAllMediumSections(req, res, next);
+}))
+
+sectionRouter.get('/small', asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    await getAllSmallSections(req, res, next);
+}))
+
+sectionRouter.get('/unit', asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    await getAllUnitSections(req, res, next);
+}))
+
+sectionRouter.get('/micro', asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    await getAllMicroSections(req, res, next);
+}))
+
 
 sectionRouter.get('/:scaleLevel', asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     await getSections(req, res, next);
